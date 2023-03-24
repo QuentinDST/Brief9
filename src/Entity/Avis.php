3fd29148455aae4a9b9eb486ea17746c8ecff770
quentin <quentin.destrade@gmail.com>
2023-03-24 14:19:48 +0100

    commit rattrapage

---
+++ src/Entity/Avis.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AvisRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AvisRepository::class)]
+class Avis
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $note = null;
+
+    #[ORM\Column(length: 500)]
+    private ?string $description = null;
+
+    #[ORM\ManyToOne(inversedBy: 'avis')]
+    private ?Utilisateur $utilisateur = null;
+
+    #[ORM\ManyToOne(inversedBy: 'avis')]
+    private ?Jeux $jeux = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getUtilisateur(): ?Utilisateur
+    {
+        return $this->utilisateur;
+    }
+
+    public function setUtilisateur(?Utilisateur $utilisateur): self
+    {
+        $this->utilisateur = $utilisateur;
+
+        return $this;
+    }
+
+    public function getJeux(): ?Jeux
+    {
+        return $this->jeux;
+    }
+
+    public function setJeux(?Jeux $jeux): self
+    {
+        $this->jeux = $jeux;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Categorie.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Jeux;
+use App\Repository\JeuxRepository;
+use App\Repository\CategorieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategorieRepository::class)]
+class Categorie
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\ManyToMany(targetEntity: Jeux::class, mappedBy: 'categorie')]
+    private Collection $jeuxes;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    public function __construct()
+    {
+        $this->jeuxes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Jeux>
+     */
+    public function getJeuxes(): Collection
+    {
+        return $this->jeuxes;
+    }
+
+    public function addJeux(Jeux $jeux): self
+    {
+        if (!$this->jeuxes->contains($jeux)) {
+            $this->jeuxes->add($jeux);
+            $jeux->addCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function removeJeux(Jeux $jeux): self
+    {
+        if ($this->jeuxes->removeElement($jeux)) {
+            $jeux->removeCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
